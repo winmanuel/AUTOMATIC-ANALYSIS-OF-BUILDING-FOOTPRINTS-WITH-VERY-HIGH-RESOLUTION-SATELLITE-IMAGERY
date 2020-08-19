29ba121ec613e1ce55887065ac89a6a434a3e812
Ervin Wirth <wirth.ervin@mailbox.org>
2020-08-19 19:45:42 +0200

    altair problem over 5000 record

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 """this is a test code"""
 import gdal
-import numpy as np
+import numpy
 # from matplotlib import pyplot as plt
 import altair as alt
 from vega_datasets import data
 import pandas as pd
+import os
 
 """ osgeo has to be installed first
 Download a proper GDAL wheel file (.whl) from here:
@@ -12,10 +13,19 @@ Download a proper GDAL wheel file (.whl) from here:
  After install it with pip: pip install path_to_wheelfile.whl
  Then the 'osgeo import' shall work"""
 
+# pip install altair_viewer
+# Needed if other renderer hadn't set up
+alt.renderers.enable('altair_viewer')
+ 
+workspace_path = os.path.dirname(__file__) 
+ 
 raster_path = r"C:\Users\user\Desktop\remote sensing\main file\AUTOMATIC-ANALYSIS-OF-BUILDING-FOOTPRINTS-" \
               r"WITH-VERY-HIGH-RESOLUTION-SATELLITE-IMAGERY\PL_PS_20200723T0742_ALL_Tile_0_0_qKSm9prB.tif"
+
+raster_path = workspace_path + "/PL_PS_20200723T0742_ALL_Tile_0_0_qKSm9prB.tif"
+              
 dataset = gdal.Open(raster_path, gdal.GA_ReadOnly)
-numpy_array = dataset.ReadAsArray().astype(np.float)
+numpy_array = dataset.ReadAsArray().astype(numpy.float)
 
 # Getting Dataset Information
 print("Driver: {}/{}".format(dataset.GetDriver().ShortName, dataset.GetDriver().LongName))
@@ -31,34 +41,39 @@ if geotransform:
 
 # for band 1
 data1 = dataset.GetRasterBand(1).ReadAsArray()
-array_band1 = np.array(data1)
+array_band1 = numpy.array(data1)
 # plt.hist(array_band1)
 # plt.show()
 
 # for band 2
 data2 = dataset.GetRasterBand(2).ReadAsArray()
-array_band2 = np.array(data2)
+array_band2 = numpy.array(data2)
 # plt.hist(array_band2)
 # plt.show()
 
 # for band 3
 data3 = dataset.GetRasterBand(3).ReadAsArray()
-array_band3 = np.array(data3)
+array_band3 = numpy.array(data3)
 # plt.hist(array_band3)
 # plt.show()
 
 # for band 4
 data4 = dataset.GetRasterBand(4).ReadAsArray()
-array_band4 = np.array(data4)
+array_band4 = numpy.array(data4)
 # plt.plot(array_band4)
 # plt.show()
 
-data = pd.DataFrame(array_band4)
-alt.Chart(data).mark_bar().encode(
-    x='x',
-    y='y',
-)
+#data = pd.DataFrame(array_band4)
+data_in_row = numpy.matrix.flatten(array_band4)
+data = {'band4': data_in_row}
+df = pd.DataFrame(data) 
 
+alt.Chart(df).mark_bar().encode(alt.X("band4", bin=alt.Bin(extent=[0, 1], step=0.05)), y='count()',)
+
+# alt.Chart(source).mark_bar().encode(
+    # alt.X("IMDB_Rating:Q", bin=True),
+    # y='count()',
+# )
 
 print("------------------------------------------------")
 """band = dataset.GetRasterBand(1)
