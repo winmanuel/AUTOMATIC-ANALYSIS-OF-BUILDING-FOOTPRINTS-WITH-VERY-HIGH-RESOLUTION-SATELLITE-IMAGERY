de34a897dc8b02d4a19c55f9241a7917fb6dd88d
winmanuel <winmanuel@users.noreply.github.com>
2020-08-20 09:12:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -93,21 +93,5 @@ print(filter_array)
 #    y='count()',
 # )
 
-print("------------------------------------------------")
-"""band = dataset.GetRasterBand(1)
-print("Band Type={}".format(gdal.GetDataTypeName(band.DataType)))
-
-
-min = band.GetMinimum()
-max = band.GetMaximum()
-if not min or not max:
-    (min, max) = band.ComputeRasterMinMax(True)
-print("Min={:.3f}, Max={:.3f}".format(min, max))
-
-if band.GetOverviewCount() > 0:
-    print("Band has {} overviews".format(band.GetOverviewCount()))
-
-if band.GetRasterColorTable():
-    print("Band has a color table with {} entries".format(band.GetRasterColorTable().GetCount()))"""
 
 # Try to some statistics first, e.g. with numpy
